55dfb6d46016238364c7b50a9b7b0d0e120555a3
Kevin Soria <60301136+kevin-soria@users.noreply.github.com>
2020-06-19 16:43:56 -0400

    more changes for exercise

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,6 @@
+import 'dart:math';
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -11,11 +14,48 @@ class BusinessCard extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.teal,
         body: SafeArea(
-          child: Container(
-            height: 100.0,
-            width: 100.0,
-            color: Colors.white,
-            child: Text('Hello World'),
+          child: Column(
+            children: <Widget>[
+              SizedBox(height: 100),
+              Container(
+                margin: EdgeInsets.symmetric(horizontal: 150.0),
+                height: 100.0,
+                width: 100.0,
+                color: Colors.white,
+                child: Text('Picture'),
+              ),
+              Spacer(),
+              Container(
+                width: 100.0,
+                height: 100.0,
+                child: Text('Name'),
+              ),
+              SizedBox(height: 20.0),
+              Container(
+                width: 100.0,
+                height: 100.0,
+                child: Text('Occupation'),
+              ),
+              SizedBox(height: 30),
+              Container(
+                width: 250.0,
+                height: 20.0,
+                child: Text('line'),
+              ),
+              SizedBox(height: 30),
+              Container(
+                width: 250.0,
+                height: 50.0,
+                color: Colors.white,
+                child: Text('Phone Number'),
+              ),
+              SizedBox(height: 30),
+              Container(
+                  width: 250.0,
+                  height: 50.0,
+                  color: Colors.white,
+                  child: Text('Email')),
+            ],
           ),
         ),
       ),
